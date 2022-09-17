a798e14ed85e960d392bf3d3e33151a3c310f54f
Brav9 <vskh93.009@gmail.com>
2022-09-17 18:59:15 +0300

    Добавление кода Talkback в GameViewModel, который должен считывать отдельные символы зашифрованного слова.

--- app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.android.unscramble.ui
 
 import android.provider.UserDictionary
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.TtsSpan
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.example.android.unscramble.ui.game.MAX_NO_OF_WORDS
 import com.example.android.unscramble.ui.game.SCORE_INCREASE
@@ -22,8 +26,21 @@ class GameViewModel : ViewModel() {
     private var wordList: MutableList<String> = mutableListOf()
     private lateinit var currentWord: String
 
-    val currentScrambledWord: LiveData<String>
-        get() = _currentScrambledWord
+    val currentScrambledWord: LiveData<Spannable> = Transformations.map(_currentScrambledWord) {
+        if (it == null) {
+            SpannableString("")
+        } else {
+            val scrambledWord = it.toString()
+            val spannable: Spannable = SpannableString(scrambledWord)
+            spannable.setSpan(
+                TtsSpan.VerbatimBuilder(scrambledWord).build(),
+                0,
+                scrambledWord.length,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            spannable
+        }
+    }
 
     init {
         Log.d("GameFragment", "GameViewModel created!")
@@ -34,7 +51,7 @@ class GameViewModel : ViewModel() {
         currentWord = allWordsList.random()
         val tempWord = currentWord.toCharArray()
         tempWord.shuffle()
-        
+
         while (String(tempWord).equals(currentWord, false)) {
             tempWord.shuffle()
         }
